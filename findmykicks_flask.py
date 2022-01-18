f106b2f85bf14712ca2168e18c7684def0f14be7
manoj nilanga <manojnilangadesilva@gmail.com>
2022-01-18 21:46:36 +0530

    vesrion 1 ebay

--- findmykicks_flask.py
+++ findmykicks_flask.py
@@ -1,6 +1,61 @@
 from flask import Flask, render_template,request
+import requests
+from bs4 import BeautifulSoup #pip install beautifulsoup4
 
 app = Flask(__name__)
+
+def middleof(text, left,right,multi=False):
+    if(not multi):
+        try:
+            middle = text.split(left)[1].split(right)[0]
+            return middle
+        except:
+            return ""
+    else:
+        right_text=text
+        items=[]
+        while(True):
+            try:
+                item = right_text.split(left)[1].split(right)[0]
+                items.append(item)
+                right_text = right_text.split(left,1)[1].split(right,1)[1]
+            except:
+                break
+        return items
+
+def scrape_items(url):
+    print("search url: "+url)
+    response = requests.get(url)
+    items = middleof(response.text, "class=s-item__link href=", "?", multi=True)
+    print(items)
+
+    item_count = 0
+    shoes_data = []
+    for item in items:
+        if (item_count > 9):
+            break
+
+        try:
+            shoe = {}
+            print("---------")
+            print("item url -> "+item)
+            response = requests.get(item)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            name = soup.find("h1", {"id": "itemTitle"}).text.replace('Details about', '').strip()
+            print(name)
+            shoe["name"] = name
+            img = soup.find("img", {"id": "icImg"})["src"]
+            print(img)
+            shoe["img"] = img
+            price = soup.find("span", {"id": "prcIsum"}).text.strip()
+            print(price)
+            shoe["price"] = price
+            shoes_data.append(shoe)
+            item_count += 1
+        except Exception as e:
+            print("Error ---> " + str(e))
+
+    return shoes_data
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
@@ -8,20 +63,17 @@ def hello_world():
     if request.method == 'GET':
         return render_template('FindMyKicks.html')
     if request.method == 'POST':
-        shoe_data = [
-            {
-                'name':'Nike Air Jordan 1 Mid Banned Black Red White 554724-074 Mens and GS New',
-                'img': 'https://i.ebayimg.com/images/g/mw4AAOSw-2JhvrjU/s-l500.jpg',
-                'price': 'US $129.99'
-            },
-            {
-                'name': "Nike Air Force 1 07 Low Triple Men's White 7-13 Casual CW2288-111 Shoes Sneakers",
-                'img': 'https://i.ebayimg.com/images/g/8wgAAOSwQy1hLFZo/s-l500.jpg',
-                'price' : 'US $67'
-            }
-        ]
-        return render_template('FindMyKicks.html',shoe_data=shoe_data)
+        try:
+            search_text = request.form["search-text"]
+            if(search_text.strip()==""):
+                return render_template('FindMyKicks.html')
+            search_url = "https://www.ebay.com/sch/i.html?_nkw="+search_text.replace(" ","+")
+            shoe_data = scrape_items(search_url)
 
+            return render_template('FindMyKicks.html',shoe_data=shoe_data)
+        except Exception as e:
+            print("Error in POST method---> " + str(e))
+            return render_template('FindMyKicks.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
--- scrape_ebay.py
+++ scrape_ebay.py
@@ -21,14 +21,35 @@ def middleof(text, left,right,multi=False):
                 break
         return items
 
-# url ="https://www.ebay.com/sch/i.html?_nkw=nike+shoes"
-# response = requests.get(url)
-# soup = BeautifulSoup(response.text, 'html.parser')
-# items = middleof(response.text, "class=s-item__link href=","?",multi=True)
-# print(items)
+url ="https://www.ebay.com/sch/i.html?_nkw=nike+shoes"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+items = middleof(response.text, "class=s-item__link href=","?",multi=True)
+print(items)
 
+item_count = 0
+shoes_data = []
+for item in items:
+    if(item_count>9):
+        break
 
-item = "https://www.ebay.com/itm/234342309373"
+    try:
+        shoe = {}
+        print("---------")
+        print(item)
+        response = requests.get(item)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find("h1", {"id": "itemTitle"}).text.replace('Details about','').strip()
+        print(name)
+        shoe["name"]= name
+        img = soup.find("img", {"id": "icImg"})["src"]
+        print(img)
+        shoe["img"] = img
+        price = soup.find("span", {"id": "prcIsum"}).text.strip()
+        print(price)
+        shoe["price"] = price
+        shoes_data.append(shoe)
+        item_count += 1
+    except Exception as e:
+        print("Error ---> "+str(e))
 
-response = requests.get(item)
-print(response.text)
